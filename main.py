db90e6b859f55ad2e5e94a37925a6bafe2ff19a5
pavlov <pavlov@megaputer.com>
2020-02-08 19:58:37 +0300

    directory

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+class httpServer(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory = "public", **kwargs)
+        
+    def do_GET(self):
+        super().do_GET()
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+Handler = httpServer;
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
