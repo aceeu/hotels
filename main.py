82cf5c4899e599bc58cbc287cfd6e547fe506794
pavlov <pavlov@megaputer.com>
2020-02-10 19:07:14 +0300

    CGI-init

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import http.server
+from http.server import HTTPServer
 import socketserver
 
 PORT = 8000
 
-class httpServer(http.server.SimpleHTTPRequestHandler):
+class httpServer(http.server.CGIHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory = "public", **kwargs)
         
-    def do_GET(self):
-        super().do_GET()
-
 Handler = httpServer;
 
 if __name__ == '__main__':
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
+    httpd = HTTPServer(("", PORT), Handler)
+    httpd.cgi_directories = "cgi-bin"
+    print("serving at port", PORT)
+    httpd.serve_forever()
